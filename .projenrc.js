6c3aa6b871fb4dd97164d9724d81871f34b79d8f
Court Schuett <schuettc@amazon.com>
2023-08-03 09:49:44 -0500

    fix: update upgrade

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,9 @@
 const { awscdk } = require('projen');
+const { JobPermission } = require('projen/lib/github/workflows-model');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.65.0',
   defaultReleaseBranch: 'main',
@@ -7,12 +12,12 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   author: 'Court Schuett',
   copyrightOwner: 'Amazon.com, Inc.',
   authorAddress: 'https://aws.amazon.com',
-  defaultReleaseBranch: 'main',
   appEntrypoint: 'amazon-chime-sdk-sma-meeting-dialer.ts',
   depsUpgradeOptions: {
     ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
     },
   },
   autoApproveOptions: {
@@ -23,7 +28,6 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   devDeps: ['esbuild'],
   deps: ['cdk-amazon-chime-resources@latest', 'fs-extra', '@types/fs-extra'],
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
-  defaultReleaseBranch: 'main',
 });
 
 const common_exclude = [
@@ -33,6 +37,47 @@ const common_exclude = [
   'dependabot.yml',
   '.DS_Store',
 ];
+const upgradeSite = project.github.addWorkflow('upgrade-site');
+upgradeSite.on({ schedule: [{ cron: '0 5 * * 1' }], workflowDispatch: {} });
+upgradeSite.addJobs({
+  upgradeSite: {
+    runsOn: ['ubuntu-latest'],
+    name: 'upgrade-site',
+    permissions: {
+      actions: JobPermission.WRITE,
+      contents: JobPermission.READ,
+      idToken: JobPermission.WRITE,
+    },
+    steps: [
+      { uses: 'actions/checkout@v3' },
+      {
+        name: 'Setup Node.js',
+        uses: 'actions/setup-node@v3',
+        with: {
+          'node-version': '16',
+        },
+      },
+      { run: 'cd site' },
+      { run: 'yarn install --check-files --frozen-lockfile' },
+      { run: 'yarn upgrade' },
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v4',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
+          'commit-message': 'chore: upgrade site',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade site',
+          'body': 'This PR upgrades site',
+          'labels': 'auto-merge, auto-approve',
+          'author': 'github-actions <github-actions@github.com>',
+          'committer': 'github-actions <github-actions@github.com>',
+          'signoff': true,
+        },
+      },
+    ],
+  },
+});
 
 project.addTask('launch', {
   exec: 'yarn && yarn projen && yarn build && yarn cdk bootstrap && yarn cdk deploy --require-approval never && yarn configLocal',
